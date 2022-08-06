62af2686eea0daaddbc67ccfa608ca79de8cb22c
jorneylopez <jorney95@live.com>
2022-08-06 14:36:45 -0600

    Layout and  Header with styles

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "./Header";
 const Layout = ({ children, pagina }) => {
   return (
     <div>
@@ -6,6 +7,7 @@ const Layout = ({ children, pagina }) => {
         <title>Guitar LA - {pagina}</title>
         <meta name="description" content="Sitio Web de Venta de Guitarras" />
       </Head>
+      <Header />
       {children}
     </div>
   );
