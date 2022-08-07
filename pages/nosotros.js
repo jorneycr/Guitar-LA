c93f802e5b281cac6945cb3f5dbb2b740a54fbeb
jorneylopez <jorney95@live.com>
2022-08-07 00:14:57 -0600

    nosotros page and style

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,9 +1,38 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
+import styles from "../styles/Nosotros.module.css";
 
 const Nosotros = () => {
   return (
     <Layout pagina="Nosotros">
-      <h1>Nosotros</h1>
+      <main className="contenedor">
+        <h2>Nosotros</h2>
+        <div className={styles.contenido}>
+          <Image
+            layout="responsive"
+            width={600}
+            height={450}
+            src="/img/nosotros.jpg"
+            alt="Imagen sobre nosotros"
+          />
+          <div>
+            <p>
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised words which don't look even
+              slightly believable. If you are going to use a passage of Lorem
+              Ipsum, you need to be sure there isn't anything embarrassing
+              hidden in the middle of text. All the Lorem Ipsum generators on
+              the Internet tend to repeat predefined chunks as necessary, making
+              this the first true generator on the Internet. It uses a
+              dictionary of over 200 Latin words, combined with a handful of
+              model sentence structures, to generate Lorem Ipsum which looks
+              reasonable. The generated Lorem Ipsum is therefore always free
+              from repetition, injected humour, or non-characteristic words etc
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
